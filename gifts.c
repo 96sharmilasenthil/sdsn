be0c2157fe0d3d0174ddda6e867eddc1a527b687
96sharmilasenthil <sharmilasenthil96@gmail.com>
2016-06-30 15:58:54 +0530

    Create gifts.c

---
+++ gifts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the height of the shot\n");
+  scanf("%d",&a);
+  if(a==67)
+    printf("Hurray !!!");
+  else if(a==47)
+    printf(":-(Aim High");
+  else if(a==120)
+    printf(":-(Aim Low");
+  return 0;
+}
