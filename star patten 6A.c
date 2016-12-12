6700afd4df37aee460b6ad34bfc0ab2598959fc6
96sharmilasenthil <sharmilasenthil96@gmail.com>
2016-12-12 15:27:42 +0530

    Create star patten 6A.c

---
+++ star patten 6A.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,j;
+    scanf("%d%d",&m,&n);
+    for(i=1;i<=m;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
