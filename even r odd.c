99e1683d44f6ec7839613f0090624487ffe1e1b4
96sharmilasenthil <sharmilasenthil96@gmail.com>
2016-12-19 13:45:42 +0530

    Create even r odd.c

---
+++ even r odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  {
+    if(a/2==0)
+      {
+        printf("Enter even");
+      }
+   else
+     {
+        printf("Enter odd");
+     }
+     }
+     return 0;
+     }
