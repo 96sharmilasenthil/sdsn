cbf936b6294bdac3ca41853cb5a5e46ab2028eea
96sharmilasenthil <sharmilasenthil96@gmail.com>
2016-12-19 14:08:33 +0530

    Create larger.c

---
+++ larger.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int n,m,o;
+scanf("%d%d%d",&n&m&o);
+{
+ if(n>m)
+ {
+   printf("Its larger");
+ }
+ else if(m>o)
+   {
+      printf("Its larger");
+   }
+else
+  {
+     printf("Its larger");
+  }
+  }
+  return 0;
+  }
